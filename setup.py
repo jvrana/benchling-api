4aca18a0494d920d4b3187d887c296276c1a08c6
jvrana <justin.vrana@gmail.com>
2016-06-14 13:31:35 -0700

    change to setup requirements

--- setup.py
+++ setup.py
@@ -16,11 +16,7 @@ config = {
     'download_url': 'https://github.com/klavinslab/benchling-api.git',
     'author_email': 'justin.vrana@gmail.com',
     'version': '0.0.3',
-<<<<<<< HEAD
-    'install_requires': ['requests'], #, 'aquariumapi'],
-=======
     'install_requires': ['requests'] #, 'aquariumapi'],
->>>>>>> master
     #'extras_require': {'benclingportal': ['coral', 'aquariumapi'],
     'packages': ['benchlingapi'],
     'scripts': [],
