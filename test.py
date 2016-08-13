413b92c3a29a21d684a3e9d71973ad870a7c454c
jvrana <jvrana@uw.edu>
2016-08-13 14:24:35 -0700

    .

--- test.py
+++ test.py
@@ -4,89 +4,17 @@ Created on Tue Dec 22 07:48:14 2015
 
 @author: Justin
 """
-from benchlingapi import BenchlingAPI, BenchlingAPIException, \
-    AquariumLoginError, BenchlingLoginError, BenchlingPortal
-import coral as cor
+from benchlingapi import BenchlingAPI, BenchlingAPIException
 
 from nose.tools import assert_equal, assert_true, assert_raises
 
 bench_api_key = 'sk_GbNYfhnukDU30J5fAebIjEj0d4YlJ'
-aq_api_key = 'GwZdTb4jr8YL3wwmVi1QYfG6jeLzUYxkLSZ7BAIKnOc'
-aq_user = 'vrana'
-aq_url = 'http://54.68.9.194:81/api'
-credentials = [bench_api_key, aq_url, aq_user, aq_api_key]
-portal = BenchlingPortal(*credentials)
+
+api = None
 
 def testBenchlingAPIConstruction():
     bench_api_key = 'sk_g7fo2vxkNUYNPkShOFIOmtY9ejIGE'
-    aq_api_key = 'GwZdTb4jr8YL3wwmVi1QYfG6jeLzUYxkLSZ7BAIKnOc'
-    aq_user = 'vrana'
-    aq_url = 'http://54.68.9.194:81/api'
-    credentials = [bench_api_key, aq_url, aq_user, aq_api_key]
-    portal = BenchlingPortal(*credentials)
-    print "Construction Test Passed"
-    
-    credentials = [bench_api+"G", aq_url, aq_user, aq_api_key]
-    assert_raises(BenchlingLoginError, BenchlingPortal, *credentials)
-    print "Benchlingloginerror test passed"
-    
-    credentials = [bench_api, aq_url+"G", aq_user, aq_api_key]
-    assert_raises(AquariumLoginError, BenchlingPortal, *credentials)
-    print "AqLoginError test 1 passed"
-    
-    credentials = [bench_api, aq_url, aq_user+"G", aq_api_key]
-    assert_raises(AquariumLoginError, BenchlingPortal, *credentials)
-    print "AqLoginError test 2 passed"
-    
-    credentials = [bench_api, aq_url, aq_user, aq_api_key+"G"]
-    assert_raises(AquariumLoginError, BenchlingPortal, *credentials)
-    print "AqLoginError test 3 passed"
+    global api
+    api = BenchlingAPI(bench_api_key)
 
-
-def testBenchlingAquariumPortal():
-    bench_api = 'sk_g7fo2vxkNUYNPkShOFIOmtY9ejIGE'
-    portal = BenchlingPortal(bench_api, 'GwZdTb4jr8YL3wwmVi1QYfG6jeLzUYxkLSZ7BAIKnOc', 'vrana', 'http://54.68.9.194:81/api')
-    #test on plasmid
-    c = portal.getSequenceFromAquarium(11231)
-    assert(type(c) == cor.DNA)
-    
-    #test on fragment
-    c = portal.getSequenceFromAquarium(11229)
-    assert(type(c) == cor.DNA)
-    
-    #test on yeast strain
-    assert_raises(BenchlingAPIException, portal.getSequenceFromAquarium, 11638)
-    
-    #test on plasmid with no sharelink
-    assert_raises(BenchlingAPIException, portal.getSequenceFromAquarium, 11825)
-
-    #test on plasmid with no sharelink
-    assert_raises(BenchlingAPIException, portal.getSequenceFromAquarium, 9883)
-
-    #test on misformatted sharelink plasmid
-    assert_raises(BenchlingAPIException, portal.getSequenceFromAquarium, 11691)
-
-def testGetAqFragment():
-    global portal
-    #seq = portal.getAqFragmentSequence(11229) #example of fragment with multiple primer binding sites
-    seq = portal.getAqFragmentSequence(10950)
-
-# def testAmbiquousPrimer():
-#     global portal
-#     assert_raises(cor.reaction._pcr.AmbiguousPrimingError, \
-#                   portal.getAqFragmentSequence, 11229)
-
-def testGibsonAssembly():
-    global portal
-    portal.gibsonAssemblyFromTask(14379)
-
-def testShareLink():
-    global portal
-    assert_raises(BenchlingAPIException, portal.getSequenceFromAquarium, 4008)
-    portal.getSequenceFromAquarium(5884)
-
-# testGetAqFragment()
-# testShareLink()
-# testGibsonAssembly()
-# testShareLink()
-print portal.AqAPI._request('modify', 9990)
+def testCreateFolder():
