0beba54f261ebdc4918313a40d8dc19493397ca4
jvrana <jvrana@uw.edu>
2016-08-25 08:37:54 -0700

    added methods for seqrecord >> benching conversion

--- benchlingapi/convert.py
+++ benchlingapi/convert.py
@@ -66,3 +66,76 @@ def write_to_gb(seqrecord, filename):
     with open(filename, 'w') as handle:
         SeqIO.write(seqrecord, handle, 'genbank')
         handle.close()
+
+
+def _get_benchlingfeatures_from_seqrecord(seqrec):
+    annotations = []
+    for feature in seqrec.features:
+        location = feature.location
+        qualifiers = feature.qualifiers
+
+        start = location.start.position
+        end = location.end.position
+        type = feature.type
+        strand = feature.strand
+        if strand not in [-1, 1]:
+            strand = 1
+        try:
+            name = qualifiers['label']
+        except:
+            name = 'unlabeled'
+        try:
+            color = qualifiers['color']
+        except:
+            color = '#F58A5E'
+
+        annotation = {
+            'start': start,
+            'end': end,
+            'type': type,
+            'strand': strand,
+            'name': name,
+            'color': color
+        }
+        annotations.append(annotation)
+    return annotations
+
+def _seqrecord_to_benchling(seqrec, default_circular=True):
+    seqrec = copy.deepcopy(seqrec)
+    _clean_seqrecord_features(seqrec)
+    annotations = _get_benchlingfeatures_from_seqrecord(seqrec)
+    bases = seqrec.seq.tostring()
+    circular = default_circular
+    assert isinstance(default_circular, bool)
+    try:
+        circular = seqrec.circular
+    except:
+        print "Could not determine topology, defaulted to {}".format(default_circular)
+        pass
+    # force with argument
+    aliases = seqrec.dbxrefs
+    description = seqrec.description
+    folder = 'argument'
+    name = seqrec.name
+    try:
+        seqrec.annotations['full_name']
+    except:
+        pass
+    bseq_id = seqrec.id # add to aliases?
+    aliases.append(bseq_id)
+    aliases = list(set(aliases))
+    bseq = dict(
+        annotations=annotations,
+        description=description,
+        folder=None,
+        aliases=aliases,
+        bases=bases,
+        circular=circular,
+        name=name
+    )
+    return bseq
+
+def save_seqrecord_to_benchling(seqrec, folder, api):
+    bseq = _seqrecord_to_benchling(seqrec)
+    bseq['folder'] = folder
+    api._post('sequences/', bseq)
