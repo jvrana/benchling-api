bdc2e6f7f4bb13f83424b5ae51b929bd5d06335c
jvrana <justin.vrana@gmail.com>
2017-10-22 14:42:05 -0700

    updates to setup

--- benchlingapi/__init__.py
+++ benchlingapi/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "1.0.dev"
+
 from .benchlingapi import BenchlingAPI, BenchlingAPIException, AquariumLoginError, BenchlingLoginError
 from .convert import *
 #from .benchlingportal import BenchlingPortal
--- setup.py
+++ setup.py
@@ -1,21 +1,52 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import os
+import re
+from distutils.core import setup
 
-config = {
-    'description': 'benchlingapi',
-    'author': 'Justin D Vrana',
-    'url': 'https://github.com/klavinslab/benchlingapi',
-    'download_url': 'https://github.com/klavinslab/benchlingapi.git',
-    'author_email': 'justin.vrana@gmail.com',
-    'version': '0.9.1',
-    'install_requires': ['requests', 'bs4'],
-    'packages': ['benchlingapi'],
-    'tests_require': ['pytest'],
-    'scripts': [],
-    'name': 'benchlingapi',
-    'license': 'Copyright University of Washington'
-}
+# about
+__author__ = 'Justin Dane Vrana'
+__license__ = 'MIT'
+__package__ = "benchlingapi"
+__readme__ = "README"
 
-setup(**config)
+tests_require = [
+    'pytest',
+    'pytest-runner',
+    'python-coveralls',
+    'pytest-pep8'
+]
+
+install_requires = ['requests', 'bs4']
+
+# setup functions
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
+    if result:
+        return result.group(1)
+    else:
+        raise RuntimeError("Unable to find property {0} in project \"{1}\".".format(prop, project))
+
+def get_version():
+    try:
+        return get_property("__version__", __package__)
+    except RuntimeError as e:
+        raise RuntimeError("Unable to find __version__ string in project \"{0}\"".format(__package__))
+
+# setup
+setup(
+        name=__package__,
+        version=get_version(),
+        packages=[__package__],
+        url='https://github.com/klavinslab/benchling-api',
+        license=__license__,
+        author=__author__,
+        author_email='justin.vrana@gmail.com',
+        keywords='api benchling dna sequence wrapper',
+        description='Intuitive API wrapper framework for Benchling',
+        long_description=read(__readme__),
+        install_requires=install_requires,
+        python_requires='>=3.4',
+        tests_require=tests_require,
+)
