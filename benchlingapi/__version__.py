5ff104ac634df14acf7d22711445304fcc86ad6f
jvrana <justin.vrana@gmail.com>
2018-09-02 09:56:51 -0700

    development version

--- benchlingapi/__version__.py
+++ benchlingapi/__version__.py
@@ -3,6 +3,6 @@
 __title__ = 'BenchlingAPI'
 __description__ = 'Shallow wrapper for the BenchlingAPI (https://docs.benchling.com)'
 __url__ = 'http://klavinslab.org/benchlingapi'
-__version__ = '2.0.0b0'
+__version__ = '2.0.0-development'
 __author__ = 'Justin D Vrana'
 __author_email__ = "justin.vrana@gmail.com"
