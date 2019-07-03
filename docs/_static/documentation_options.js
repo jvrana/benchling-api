2878aa30d5caa89bd968f79e12d036fa8ff93787
Justin Vrana <justin.vrana@gmail.com>
2019-07-02 22:32:22 -0700

    release (hotfix) - updating docs for release 2.0.2a

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.1a',
+    VERSION: '2.0.2a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
