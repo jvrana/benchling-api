b92cdf8e12f02d344c2a3b5817bea2904fdd6261
Justin Vrana <justin.vrana@gmail.com>
2020-02-12 16:33:31 -0800

    update org and home tests

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,3 +1,16 @@
-#
+from benchlingapi import Session
+
+
 def test_session(session):
     pass
+
+
+def test_setting_org():
+    session = Session('alsdfja;lsdfj', org='myorganization')
+    assert session.url == 'https://myorganization.benchling.com/api/v2'
+
+
+def test_setting_home():
+    home = 'https://myotherorganization.benchling.com/api/v2'
+    session = Session('alsdfja;lsdfj', home=home)
+    assert session.url == home
